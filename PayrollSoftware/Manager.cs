3c13c485504d2441088302ccc9de05923d372eb0
Kent Moseley <moseleykent856@gmail.com>
2021-02-18 23:53:12 -0500

    Manager Class

---
+++ PayrollSoftware/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSoftware
+{
+    class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+        
+        public int Allowance { get; private set; }
+        public Manager(string name) : base(name, managerHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 1000;
+        }
+    }
+}
