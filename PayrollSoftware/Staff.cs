3c2eec52b69f3899e12d21bed1ecad9c97a224e7
BlankCloud2021 <78029266+BlankCloud2021@users.noreply.github.com>
2021-02-18 20:07:06 -0500

    Add project files.

---
+++ PayrollSoftware/Staff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollSoftware
+{
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        protected float TotalPay { get; set; }
+        private float BasicPay { get; set; }
+
+        private string NamOfStaff { get; set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    hWorked = value;
+                }
+                hWorked = 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            NamOfStaff = name;
+
+            hourlyRate = rate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return $"\n NameOfStaff = {NamOfStaff}\n hourlyRate = {hourlyRate}\n hWorked = {hWorked} \n BasicPay = {BasicPay}\n TotalPay = {TotalPay}";
+        }
+        
+    }
+}
